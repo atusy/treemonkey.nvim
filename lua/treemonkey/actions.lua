9203f2457b5f6b19288541199bf167a0ad09cf96
atusy <30277794+atusy@users.noreply.github.com>
2023-12-29 11:28:34 +0900

    feat: add unite_selection and jump actions

--- lua/treemonkey/actions.lua
+++ lua/treemonkey/actions.lua
@@ -45,4 +45,48 @@ function M.update_selection(item)
 	_update_selection(item)
 end
 
+---@type TreemonkeyAction
+function M.unite_selection(item)
+	set_mode()
+	local o1 = vim.api.nvim_win_get_cursor(0)
+	vim.cmd("normal! o")
+	local o2 = vim.api.nvim_win_get_cursor(0)
+	vim.cmd("normal! o")
+	if o1[1] == o2[1] and o1[2] == o2[2] then
+		return M.update_selection(item)
+	end
+
+	local on = "start"
+	local vtop, vbot = o1, o2
+	if o1[1] > o2[1] or ((o1[1] == o2[1]) and (o1[2] < o2[2])) then
+		on = "end"
+		vtop, vbot = o1, o2
+	end
+
+	local vsrow, vscol, verow, vecol = vtop[1] - 1, vtop[2], vbot[1] - 1, vbot[2]
+	local nsrow, nscol, nerow, necol = range(item.node)
+
+	if nsrow < vsrow or (nsrow == vsrow and nscol < vscol) then
+		if nerow > verow or (nerow == verow and necol > vecol) then
+			return _update_selection(item)
+		end
+		if on == "end" then
+			vim.cmd("normal! o")
+		end
+		vim.api.nvim_win_set_cursor(0, { nsrow + 1, nscol })
+		return
+	elseif nerow > verow or (nerow == verow and necol > necol) then
+		if on == "start" then
+			vim.cmd("normal! o")
+		end
+		vim.api.nvim_win_set_cursor(0, { nerow + 1, necol })
+		return
+	end
+end
+
+---@type TreemonkeyAction
+function M.jump(item)
+	vim.api.nvim_win_set_cursor(0, { item.row + 1, item.col })
+end
+
 return M
